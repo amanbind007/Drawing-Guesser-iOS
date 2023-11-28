c993c57458d9dc1c2d9749cce70362201c58a872
Aman <amanbind007@gmail.com>
2023-11-28 17:21:53 +0530

    Initial commit

---
+++ Drawing Guesser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Drawing Guesser
+//
+//  Created by Aman Bind on 28/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Drawing Guesser/Drawing_GuesserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drawing_GuesserApp.swift
+//  Drawing Guesser
+//
+//  Created by Aman Bind on 28/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Drawing_GuesserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
